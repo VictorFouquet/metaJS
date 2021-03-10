eedb36de84658ec95691255c0e0c3f435bc6e0f1
victor.fouquet <victor.fouquet@gmail.com>
2021-03-10 20:56:09 +0100

    implementing LRU cache with Proxy

--- proxy.js
+++ proxy.js
@@ -1,3 +1,6 @@
+// Defines properties that should be ignored when caching
+const PROPS_TO_IGNORE = ['stack', 'capacity', 'cache', 'delete', 'size'];
+
 function defineCacheProperties(capacity, init) {
   /**
    * Defines cache's properties.
@@ -26,74 +29,116 @@ function defineCacheProperties(capacity, init) {
   // to further define a new property into the cache.
   Reflect.defineProperty(init, 'cache', {
     value: function(key, val) {
-      this[key] = val;
+      Reflect.set(this, key, val);
       return this;
     },
     enumerable: false
   })
+  Reflect.defineProperty(init, 'delete', {
+    value: function(key) {
+      return Reflect.deleteProperty(this, key);
+    }, enumerable: false}
+  )
+}
+
+function removeDoubles(target, prop) {
+  /**
+   * Prevents cache's stack to store doubles.
+   * @param {target} object
+   * @param {prop}   string
+   */
+  // Finds index of the prop to remove in stack.
+  let idx = target.stack.indexOf(prop);
+  // Removes item from stack if it's yet stored.
+  if (idx !== -1) target.stack.splice(idx, 1);
+}
+
+function updateLRUStack(target, prop) {
+  /**
+   * Keeps track of least recently used prop.
+   * @param {target} object
+   * @param {prop}   string
+   */
+  // Removes doubles from stack then pushes used prop.
+  if (target[prop]) removeDoubles(target, prop);
+  target.stack.push(prop);
+}
+
+function getHandler(target, prop) {
+  /**
+   * Handles get queries.
+   * @param {target} object
+   * @param {prop}   string
+   */
+  if (!PROPS_TO_IGNORE.includes(prop)) {
+    // Updates least recently used stack.
+    updateLRUStack(target, prop)
+  }
+  return target[prop]
+}
+
+function setHandler(target, prop, val) {
+  /**
+   * Handles set queries.
+   * @param {target} object
+   * @param {prop}   string
+   * @param {val}    integer
+   */
+  if (prop == 'capacity') {
+    // When setting capacity, delete overflowing props
+    // according to new cache's capacity.
+    target[prop] = val;
+    while (target.size >= target.capacity) {
+      delete target[target.stack.shift()]
+      target.size --;
+    }
+
+  } else if (!PROPS_TO_IGNORE.includes(prop)) {
+    updateLRUStack(target, prop)
+    // Unshift prop from stack and deletes prop if cache is full.
+    if (target.size >= target.capacity && !target[prop]) {
+      delete target[target.stack.shift()];
+      target.size --;
+    }
+    // Increases cache's size and store the prop its value.
+    if (!target[prop]) target.size++;
+    target[prop] = val
+  }
+}
+
+function deleteHandler(target, prop) {
+  /**
+   * Handles delete queries.
+   * @param {target} object
+   * @param {prop}   string
+   */
+  if (!PROPS_TO_IGNORE.includes(prop)) {
+    // Removes prop from cache's stack.
+    target.stack.splice(
+      target.stack.indexOf(prop), 1
+    );
+    // Decreases cache's size and delete prop.
+    if (Reflect.has(target, prop)) target.size --;
+    return Reflect.deleteProperty(target, prop);
+  }
 }
 
 function LRUCache(capacity, init) {
-  // Defines properties that should be ignored when caching
-  const propsToIgnore = ['stack', 'capacity', 'cache', 'delete', 'size'];
-  
-  // Helper that prevents from having doubles inside the LRU stack.
-  function removeDoubles(target, prop) { 
-    let idx = target.stack.indexOf(prop);
-    if (idx !== -1) target.stack.splice(idx, 1);
-  }
-  
+  /**
+   * @param {capacity} number
+   * @param {init}     object
+   */
   defineCacheProperties(capacity, init);
-  
-  Reflect.defineProperty(init, 'delete', {
-    value: function(a) {
-      if (
-        Object.keys(this).includes(a) && 
-        !propsToIgnore.filter(prop=>prop!='size').includes(a)
-      ) {
-        this.stack.splice(this.stack.indexOf(a), 1)
-        delete this[a];
-        this.size = this.size-1;
-      }
-      return !propsToIgnore.includes(a);
-    }, enumerable: false}
-  )
-  
+  // Handles cache's intercation with a proxy.
   return new Proxy(init, {
-    get: function(target, prop) {
-      if (!propsToIgnore.includes(prop) && target[prop]) {
-        removeDoubles(target, prop);
-        target.stack.push(prop);
-      }
-      return target[prop]
+    get: function(target, prop) { 
+      return getHandler(target, prop) 
     },
-    set: function(target, prop, val) {
-      if (prop == 'capacity') {
-        target.capacity = val;
-        while (target.size > target.capacity) {
-          delete target[target.stack.shift()];
-          target.size --;
-        }
-      } else if (prop == 'size') {
-        target.size = val;
-      } else if (!propsToIgnore.includes(prop)) {
-        removeDoubles(target, prop);
-        target.stack.push(prop)
-        if (target.size >= target.capacity && !target[prop]) {
-          delete target[target.stack.shift()];
-          target.size --;
-        }
-        if (!target[prop]) target.size++;
-        target[prop] = val
-      }
+    set: function(target, prop, val) { 
+      return setHandler(target, prop, val) 
+    },
+    deleteProperty: function(target, prop) { 
+      return deleteHandler(target, prop)
     }
   })
 }
-
-lru = new LRUCache(3, {a:0})
-lru.b = 5
-lru.c = 11
-console.log(lru.capacity, lru.a, lru.b, lru.stack)
-lru.d = 5
-console.log(lru.stack)
-console.log(lru.c)
